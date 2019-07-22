4bcd2e0f4e2016104737b7868c9081dde147b458
Huy Do <huyddo40@gmail.com>
2019-07-22 04:33:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,12 +21,18 @@ class Song
     # @@all << song
     # song
     #
-    song_name = filename.split(" - ")[1]
-    song = Song.new(song_name)
-    artist_object = Artist.find_or_create_by_name(filename.split(" - ")[0])
-    song.artist = artist_object
-    song.artist.add_song(self)
-    song
+    # song_name = filename.split(" - ")[1]
+    # song = Song.new(song_name)
+    # artist_object = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    # song.artist = artist_object
+    # song.artist.add_song(self)
+    # song
+
+    artist, title = filename.split(" - ")
+    self.new(title).tap{|song|
+      song.artist = Artist.find_or_create_by_name(aritst)
+      song.artis.add_song(song)
+    }
   end
 
 end
